ddde39848bcf2779a1e79aeeda36e20c3f5ea581
Paulson Jose <paul@hackermail.com>
2023-08-26 01:06:57 +0530

    Create userRoutes and JWT auth

--- server/server.js
+++ server/server.js
@@ -1,25 +1,29 @@
 import express from 'express'
 import connectDB from './config/db.js'
 import dotenv from 'dotenv'
-// import cors from 'cors'
+import cookieParser from 'cookie-parser'
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 import { notFound, errorHandler } from './middleware/errorHandler.js'
 
 
 dotenv.config()
 const port = process.env.PORT
 
-const app = express()
-
 connectDB()
 
-// app.use(cors())
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
 
 app.get('/', (req, res)=>{
-    res.send('server at 3000')
+    res.send('server at 5001')
 })
 
 app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
 
 app.use(notFound)
 app.use(errorHandler)
